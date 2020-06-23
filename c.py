5c51096569977636e8a8dd6f909347c4720f71ca
dina alaa <dinaalaaahmed@gmail.com>
2020-06-23 19:01:46 +0200

    reading inputs, file comments

--- c.py
+++ c.py
@@ -4,53 +4,71 @@ import numpy as np
 import time
 
 startTime = time.time()
-# read inputs
 
-blockSize = 3
-size = 1
-typeOfFloat = 'float64'
 
-# inputs validation
 image_name = 'r.bmp'
 
 
-# read image as gray
+# read image
 img = cv2.imread(image_name, 1)
-height,wedth,channels=img.shape
+height, wedth, channels = img.shape
 
 
-arrheightRed=[]
-arrheightGreen=[]
-arrheightBlue=[]
+# read inputs
+print("please enter the block size of arithmetic coding (integer number): ")
+blockSize = input()
+
+print("please enter the float type of the numpy array ('float64', 'float32', 'float16'): ")
+typeOfFloat = input()
+
+arr_width = []
+arr_height = []
+
+for x in range(10):
+    if height % (x+1) == 0:
+        arr_height.append(x+1)
+    if wedth % (x+1) == 0:
+        arr_width.append(x+1)
+
+print(arr_height)
+print("please enter the height size of the average window from the previous array: ")
+size_height = input()
+
+print(arr_width)
+print("please enter the width size  of the average window from the previous array: ")
+
+size_width = input()
+
+# average the neighbouring pixels with window size of (size_height*size_width)
+arrheightRed = []
+arrheightGreen = []
+arrheightBlue = []
 
 def channel(num):
     i = 0
     arrheight=[]
-    while i + size - 1 < height:
+    while i + size_height - 1 < height:
         j = 0
 
         arrwedth = []
-        while j + size - 1 < wedth:
+        while j + size_width - 1 < wedth:
             x = 0
-            for y in range(size):
-                if size == 1:
-                    x += img[i + y][j][num]
-                else:
-                    x += img[i + y][j][num]
-                    x += img[i][j + y][num]
-
-            j += size
-            arrwedth.append(x / (size * size))
-        i += size
+            for y in range(size_height):
+                for t in range(size_width):
+                    x += img[i + y][j + t][num]
+            j += size_width
+            arrwedth.append(x / (size_width * size_height))
+        i += size_height
         arrheight.append(arrwedth)
     return arrheight
-arrheightRed=channel(0)
-arrheightGreen=channel(1)
-arrheightBlue=channel(2)
+
+
+arrheightRed = channel(0)
+arrheightGreen = channel(1)
+arrheightBlue = channel(2)
 
 
 # flatten the image
-
 def flattenArrays(arrheight):
     flattenImage=[]
     for items in arrheight:
@@ -59,20 +77,15 @@ def flattenArrays(arrheight):
     return flattenImage
 
 
-
-flattenImageRed=flattenArrays(arrheightRed)
-flattenImageBlue=flattenArrays(arrheightBlue)
-flattenImageGreen=flattenArrays(arrheightGreen)
-
-
-
-
-
+flattenImageRed = flattenArrays(arrheightRed)
+flattenImageBlue = flattenArrays(arrheightBlue)
+flattenImageGreen = flattenArrays(arrheightGreen)
 
 # Encoding Part
 
 
-
+# first: calculate probability of pixels appearance of each channel(red, green, blue)
+# second: calculate ranges of intervals between(0,1) of each channel(red, green, blue)
 def startEnd(flattenImage):
     probability = []
 
@@ -94,16 +107,16 @@ def startEnd(flattenImage):
         start[x] = startD
         end[x] = start[x] + probability[x]
         startD = end[x]
-    return start,end
+    return start, end
 
-startRed,EndRed=startEnd(flattenImageRed)
-startBlue,EndBlue=startEnd(flattenImageBlue)
-startGreen,EndGreen=startEnd(flattenImageGreen)
+
+startRed, EndRed = startEnd(flattenImageRed)
+startBlue, EndBlue = startEnd(flattenImageBlue)
+startGreen, EndGreen = startEnd(flattenImageGreen)
+
 
 # encode the image
-
-
-def encode_arithmetic(levels,start,end,dictionary):
+def encode_arithmetic(levels, start, end, dictionary):
     lower = 0
     upper = 1
     for code in levels:
@@ -114,120 +127,128 @@ def encode_arithmetic(levels,start,end,dictionary):
     dictionary.append(lower + (upper-lower)/2.0)
 
 
+def encode_array(array, start, end):
 
-def encode_array(array,start,end):
     dictionary = []
     i = 0
-    for x in range(((height*wedth)/(size*size))/blockSize):
+    for x in range(((height*wedth)/(size_width*size_height))/blockSize):
         arr = []
         for y in range(blockSize):
             arr.append(array[i])
             i = i+1
-        encode_arithmetic(arr,start,end,dictionary)
+        encode_arithmetic(arr, start, end, dictionary)
     return dictionary
 
 
-dictionaryRed=encode_array(flattenImageRed,startRed,EndRed)
-dictionaryBlue=encode_array(flattenImageBlue,startBlue,EndBlue)
-dictionaryGreen=encode_array(flattenImageGreen,startGreen,EndGreen)
+dictionaryRed = encode_array(flattenImageRed, startRed, EndRed)
+dictionaryBlue = encode_array(flattenImageBlue, startBlue, EndBlue)
+dictionaryGreen = encode_array(flattenImageGreen, startGreen, EndGreen)
 
+# save the encoded arrays into numpy arrays
 DictionaryRed = np.array(dictionaryRed, dtype=typeOfFloat)
-np.save('dictionaryRed.npy', DictionaryRed)
 DictionaryBlue = np.array(dictionaryBlue, dtype=typeOfFloat)
-np.save('dictionaryBlue.npy', DictionaryBlue)
 DictionaryGreen = np.array(dictionaryGreen, dtype=typeOfFloat)
-np.save('dictionaryGreen.npy', DictionaryGreen)
+np.savez('dictionary.npz', array1=DictionaryRed, array2=DictionaryGreen, array3=DictionaryBlue)
 
-print("encoding time in seconds",time.time()-startTime)
+print("encoding time in seconds", time.time()-startTime)
 
-startTime=time.time()
-dictionaryRed = np.load('dictionaryRed.npy', allow_pickle=True)
-dictionaryBlue = np.load('dictionaryBlue.npy', allow_pickle=True)
-dictionaryGreen = np.load('dictionaryGreen.npy', allow_pickle=True)
+# decoding part
+startTime = time.time()
 
-def binary_search(start,end, l, h, code,lower,upper,blokSize,i,decodingDictionary):
-  if i >= blokSize:
-      return
-  if h >= l:
-    mid = l + (h - l)/2
-    d_lower = lower
-    rang = upper - lower
-    lower = d_lower + start[mid] * rang
-    upper = d_lower + end[mid] * rang
-    if lower<code<upper:
-        decodingDictionary.append(mid)
-        i+=1
-        binary_search(start,end,0,255,code,lower,upper,blokSize,i,decodingDictionary)
-    elif lower > code:
-      lower = d_lower
-      upper = rang + lower
-      return binary_search(start,end, l, mid-1, code,lower,upper,blokSize,i,decodingDictionary)
-    else:
-      lower = d_lower
-      upper = rang + lower
-      return binary_search(start,end, mid+1, h, code,lower,upper,blokSize,i,decodingDictionary)
+# read numpy arrays
+data = np.load('dictionary.npz', allow_pickle=True)
+dictionaryRed = data['array1']
+dictionaryGreen = data['array2']
+dictionaryBlue = data['array3']
+
+# decode each code into sequence of pixels using binary search
+def binary_search(start, end, l, h, code, lower, upper, blokSize, i, decodingDictionary):
+    if i >= blokSize:
+        return
+    if h >= l:
+        mid = l + (h - l) / 2
+        d_lower = lower
+        rang = upper - lower
+        lower = d_lower + start[mid] * rang
+        upper = d_lower + end[mid] * rang
+        if lower < code < upper:
+            decodingDictionary.append(mid)
+            i += 1
+            binary_search(start, end, 0, 255, code, lower, upper, blokSize, i, decodingDictionary)
+        elif lower > code:
+            lower = d_lower
+            upper = rang + lower
+            return binary_search(start, end, l, mid - 1, code, lower, upper, blokSize, i, decodingDictionary)
+        else:
+            lower = d_lower
+            upper = rang + lower
+            return binary_search(start, end, mid + 1, h, code, lower, upper, blokSize, i, decodingDictionary)
 
 
-def decode_arithmetic(dictionary,start,end):
+def decode_arithmetic(dictionary, start, end):
     decodingDictionary = []
     for i in dictionary:
-        binary_search(start, end, 0, 255, i, 0, 1, blockSize, 0,decodingDictionary)
+        binary_search(start, end, 0, 255, i, 0, 1, blockSize, 0, decodingDictionary)
     return decodingDictionary
 
 
-
-decodingDictionaryRed=decode_arithmetic(dictionaryRed,startRed,EndRed)
-decodingDictionaryBlue=decode_arithmetic(dictionaryBlue,startBlue,EndBlue)
-decodingDictionaryGreen=decode_arithmetic(dictionaryGreen,startGreen,EndGreen)
-
+decodingDictionaryRed = decode_arithmetic(dictionaryRed, startRed, EndRed)
+decodingDictionaryBlue = decode_arithmetic(dictionaryBlue, startBlue, EndBlue)
+decodingDictionaryGreen = decode_arithmetic(dictionaryGreen, startGreen, EndGreen)
 
 
-while len(decodingDictionaryRed) < height*wedth/(size*size):
+while len(decodingDictionaryRed) < height*wedth/(size_width*size_height):
     decodingDictionaryRed.append(decodingDictionaryRed[0])
-while len(decodingDictionaryGreen) < height * wedth / (size * size):
-        decodingDictionaryGreen.append(decodingDictionaryGreen[0])
-while len(decodingDictionaryBlue) < height*wedth/(size*size):
+
+while len(decodingDictionaryGreen) < height * wedth / (size_width*size_height):
+    decodingDictionaryGreen.append(decodingDictionaryGreen[0])
+
+while len(decodingDictionaryBlue) < height*wedth/(size_width*size_height):
     decodingDictionaryBlue.append(decodingDictionaryBlue[0])
 
 
-
-# # reshape decoded image and save it
+# reshape decoded image then return the average pixels then save the image
 def reshape():
-    imageRed = np.reshape(decodingDictionaryRed, (height / size, wedth / size))
-    imageGreen = np.reshape(decodingDictionaryGreen, (height / size, wedth / size))
-    imageBlue = np.reshape(decodingDictionaryBlue, (height / size, wedth / size))
+    imageRed = np.reshape(decodingDictionaryRed, (height / size_height, wedth / size_width))
+    imageGreen = np.reshape(decodingDictionaryGreen, (height / size_height, wedth / size_width))
+    imageBlue = np.reshape(decodingDictionaryBlue, (height / size_height, wedth / size_width))
 
     i = 0
-
     arrheight = []
-    while i < height / size:
+
+    while i < height / size_height:
+
         arrwedth = []
         j = 0
-        while j < wedth / size:
-            for x in range(size):
-                arrwedth.append([imageRed[i][j],imageGreen[i][j],imageBlue[i][j]])
+        while j < wedth / size_width:
+
+            for x in range(size_width):
+                arrwedth.append([imageRed[i][j], imageGreen[i][j], imageBlue[i][j]])
             j += 1
         i += 1
-        for y in range(size):
+
+        for y in range(size_height):
+
             arrheight.append(arrwedth)
 
     return arrheight
 
 
-arrheight=reshape()
-image = np.reshape(arrheight, (height,wedth,3))
-im=np.reshape((image-img),(height*wedth*3))
+arrheight = reshape()
+image = np.reshape(arrheight, (height, wedth, 3))
+im = np.reshape((image-img), (height*wedth*3))
 
-MSE=0
+# calculate MSE and PSNR
+MSE = 0
 for x in im:
-    MSE+=int(pow(x,2))
-MSE=MSE/(height*wedth*3.0)
-if(MSE!=0.0):
+    MSE += int(pow(x,2))
+MSE = MSE/(height*wedth*3.0)
+if MSE != 0.0:
     PSNR = 10 * math.log10((pow(255, 2)) / MSE)
-    print(PSNR)
+    print("PSNR:", PSNR, " db")
 
 
-print("decoding time in seconds",time.time()-startTime)
+print("decoding time in seconds", time.time()-startTime)
 
 cv2.imwrite('image.png', image)
 cv2.waitKey(0)
